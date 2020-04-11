ca26c45cba5be8c9bf7955d1f6d183722a8db85e
9831140 <9831140@ceit.local>
2020-04-11 16:42:58 +0430

    readMove method was added to player class

--- src/Player.java
+++ src/Player.java
@@ -17,5 +17,42 @@ public class Player
         this.playerColor = playerColor;
         this.board = board;
     }
-    
+    public void readMove(int tile, int position, int rotatingTile, String direction, String player)
+    {
+        boolean valid = true;
+        if (!(tile>0 && tile<5))
+        {
+            System.out.println("Wrong Tile!");
+            valid = false;
+        }
+
+        if (!(position>0 && position<10))
+        {
+            System.out.println("Wrong Position!");
+            valid = false;
+
+        }
+
+        if (!(rotatingTile>0 && rotatingTile<5))
+        {
+            System.out.println("Wrong Tile For Rotation!");
+            valid = false;
+
+        }
+
+        if (((direction.equals("right")) || (direction.equals("left"))))
+        {
+            valid = true;
+        }
+        else
+        {
+            System.out.println("Wrong Rotation Direction!");
+            valid = false;
+        }
+
+        if (valid)
+        {
+
+        }
+    }
 }
